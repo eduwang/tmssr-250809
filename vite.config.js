56b28da746a4590f9576f57905c84c067dc108b1
eduwang <145199866+eduwang@users.noreply.github.com>
2025-07-29 02:24:03 +0900

    vite config 작업

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        page1: resolve(__dirname, 'page1.html'),
+        // page2: resolve(__dirname, 'page2.html'),
+        // page3: resolve(__dirname, 'page3.html'),
+        admin: resolve(__dirname, 'admin.html')
+      },
+    },
+  },
+  define: {
+    // Firebase 일부 패키지가 process.env를 참조하는 경우가 있으므로 아래 추가
+    'process.env': {},
+  },
+});
